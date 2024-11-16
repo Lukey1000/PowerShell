8a2ecbf4ddd272ffbd73f46d73804106d89ac08f
Luke Brown <134363124+Lukey1000@users.noreply.github.com>
2024-11-17 07:46:38 +1100

    Update Xenmerge.ps1

--- Xenmerge.ps1
+++ Xenmerge.ps1
@@ -1,3 +1,13 @@
+# Powershell script to transfrom a XenMobile export (ManagedDevices) into a specific mail merge ready format.
+# By doing this rather than a mass mail, user's recceived a personalised message just to them and this noticably improved their patching.
+# The output from XenMobile device export .csv's was just awful. This script:
+# 1. Unifies version numbers to be easily sorted
+# 2. Recreates email addresses from firstname lastname domain values
+# 3. Unifies date formats
+# 4. Selects only devices that have authed in the last 30 days
+# 5. And finally outputs a mail merge ready .csv
+# Written by Luke Brown. 2022-11
+
 $domain = ‘domain.com’
 Import-Csv -Path '.\Managed Devices.csv' |
     Where-Object { $_.User -notlike "*Device Enrollment Program User*" -and $_.User -notlike "*anonymous*" } | 
